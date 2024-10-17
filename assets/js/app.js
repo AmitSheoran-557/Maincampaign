cef9ba54745f86acca250c8f007e7a9b08fb48c2
AmitSheoran-557 <amit22082006@gmail.com>
2024-10-17 14:57:46 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,83 @@
+// ========================== businesses-generate-slider-js =================================
+$(".businesses-generate-slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+    ]
+});
+//   =========================== footer-slider-js ====================================
+
+$(".slider-footer").slick({
+    dots: false,
+    speed: 2000,
+    cssEase: 'linear',
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    infinite: true,
+    swipeToSlide: true,
+    centerMode: true,
+    focusOnSelect: true,
+    nextArrow: ".next",
+    prevArrow: ".prev",
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 0,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 0,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 0,
+            }
+        },
+
+    ]
+});
